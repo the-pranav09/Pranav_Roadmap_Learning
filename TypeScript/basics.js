3a4e07e3d8e53520a1a3c06140717c2cbc57a8de
Pranav Ghadge <psghadge09@gmail.com>
2026-07-16 20:21:26 +0530

    variable value show

--- TypeScript/basics.js
+++ TypeScript/basics.js
@@ -1,2 +1,6 @@
 "use strict";
-console.log("Hello");
+// TypeScript is static type mostly.
+let a = "golu";
+let num = 45;
+console.log(a, num);
+// let val:any = 7 // dont use 'any' data_type -'any' is data_type kon sa bhi type ka data de sakte ho.
--- TypeScript/basics.ts
+++ TypeScript/basics.ts
@@ -1,5 +1,5 @@
 // TypeScript is static type mostly.
 let a:string = "golu";
 let num:number = 45;
-
+console.log(a,num)
 // let val:any = 7 // dont use 'any' data_type -'any' is data_type kon sa bhi type ka data de sakte ho.
