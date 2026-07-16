c56b4f2ee64a1b2862ce32e63930194fc2fa49e9
Pranav Ghadge <psghadge09@gmail.com>
2026-07-16 20:28:13 +0530

    perticular type of array

--- TypeScript/basics.ts
+++ TypeScript/basics.ts
@@ -3,3 +3,9 @@ let a:string = "golu";
 let num:number = 45;
 console.log(a,num)
 // let val:any = 7 // dont use 'any' data_type -'any' is data_type kon sa bhi type ka data de sakte ho.
+
+
+// Array-------------------------------------------------------------------------------------------------------- //
+// we can define type of array.(konse type ka array hai).
+let arr:number[] = [1,2,3,4,5]; // number ka array hai.
+let arr2:string[] = ["Raj", "Prem", "Goly", "Yash"] // string ka array hai.
