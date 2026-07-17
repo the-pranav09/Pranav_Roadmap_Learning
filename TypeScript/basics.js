a41a57bb13e96c345bbb12729eb2ee42e88b4fa3
Pranav Ghadge <psghadge09@gmail.com>
2026-07-17 13:44:58 +0530

    Type Inference and Annotation & also declaring variable

--- TypeScript/basics.js
+++ TypeScript/basics.js
@@ -1,6 +1,28 @@
 "use strict";
 // TypeScript is static type mostly.
-let a = "golu";
-let num = 45;
-console.log(a, num);
+// let a:string = "golu";
+// let num:number = 45;
+// console.log(a,num)
 // let val:any = 7 // dont use 'any' data_type -'any' is data_type kon sa bhi type ka data de sakte ho.
+// Data_type Declaration.
+let a = "golu";
+let num;
+num = 45;
+let c;
+c = "Hit-Man";
+console.log(c, c);
+// Array-------------------------------------------------------------------------------------------------------- //
+// we can define type of array.(konse type ka array hai).
+// let arr:number[] = [1,2,3,4,5]; // number ka array hai.
+// let arr2:string[] = ["Raj", "Prem", "Goly", "Yash"] // string ka array hai.
+// Tuple-------------------------------------------------------------------------------------------------------- //
+// Tuple is array with fixed size with specific type.
+// let arr:[string, number, boolean] = ["Golya",4, true] //size bhi fix and uss ke andar vale konsi location pe kay aayega vo bhi fixed hai.
+// Enum-------------------------------------------------------------------------------------------------------- //
+// Enum is thoda similar like 'object' that assign the readable name to values.
+// enum Role {
+//     ADMIN="admin",
+//     GUEST="guest",
+//     RANDOM="random",
+// }
+// console.log(Role.ADMIN)
--- TypeScript/basics.ts
+++ TypeScript/basics.ts
@@ -1,11 +1,49 @@
 // TypeScript is static type mostly.
-let a:string = "golu";
-let num:number = 45;
-console.log(a,num)
+// let a:string = "golu";
+// let num:number = 45;
+// console.log(a,num)
 // let val:any = 7 // dont use 'any' data_type -'any' is data_type kon sa bhi type ka data de sakte ho.
 
+// Data_type Declaration.
+// let a:string = "golu";
+// let num:number;
+// num = 45
+
+// let c: number | string;
+// c = "Hit-Man"
+// console.log(c)
 
 // Array-------------------------------------------------------------------------------------------------------- //
 // we can define type of array.(konse type ka array hai).
-let arr:number[] = [1,2,3,4,5]; // number ka array hai.
-let arr2:string[] = ["Raj", "Prem", "Goly", "Yash"] // string ka array hai.
+// let arr:number[] = [1,2,3,4,5]; // number ka array hai.
+// let arr2:string[] = ["Raj", "Prem", "Goly", "Yash"] // string ka array hai.
+
+
+// Tuple-------------------------------------------------------------------------------------------------------- //
+// Tuple is array with fixed size with specific type.
+
+// let arr:[string, number, boolean] = ["Golya",4, true] //size bhi fix and uss ke andar vale konsi location pe kay aayega vo bhi fixed hai.
+
+
+// Enum-------------------------------------------------------------------------------------------------------- //
+// Enum is thoda similar like 'object' that assign the readable name to values.
+
+// enum Role {
+//     ADMIN="admin",
+//     GUEST="guest",
+//     RANDOM="random",
+// }
+// console.log(Role.ADMIN)
+
+
+// 'Type Inference'-------------------------------------------------------------------------------------------------------- //
+// 'Inference' jab variable ko pata hai value kis type ki hai.
+// Type_Script knows the variable type automatically.
+// let a = 45
+// let b = "HIT-MAN"
+
+
+// 'Type Annotation'-------------------------------------------------------------------------------------------------------- //
+// 'Annotation' declaring the data_type to variable.(For Better Clarity).
+// let a:string = "Ro-Hit" // adding the data_type.
+// let no:number = 45
