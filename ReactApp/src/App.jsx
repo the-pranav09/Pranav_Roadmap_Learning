f7676808d9cda78d7c4b122cecf9cca7080e831e
Pranav Ghadge <psghadge09@gmail.com>
2026-07-20 16:18:09 +0530

    form handling small example

--- ReactApp/src/App.jsx
+++ ReactApp/src/App.jsx
@@ -34,25 +34,28 @@ import { useState } from "react";
 
 // Form Handling ----------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
-export const App = () => {
+// export const App = () => {
 
-  const submitHandler = (e) => {
-    e.preventDefault() // 'preventDefault()' method that is part of 'Event interface' and use for "stop" browser default behavior.(browser ke default behavior ko band krta hai).
+//   const submitHandler = (e) => {
+//     e.preventDefault() // 'preventDefault()' method that is part of 'Event interface' and use for "stop" browser default behavior.(browser ke default behavior ko band krta hai).
 
-    console.log("Hare Krishna")
-  }
+//     console.log("Hare Krishna")
+//   }
 
-  return (
-    <div>
-      <form onSubmit={(e)=>{
-        submitHandler(e)
-      }}>
-        <input className="px-4px rounded py-3 text-xl m-5 bg-white text-black" type="text" placeholder="Enter the Name"/>
-        <button className="px-4px py-3 w-50 text-xl text-white bg-emerald-600 rounded">Upload</button>
-      </form>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <form onSubmit={(e)=>{
+//         submitHandler(e)
+//       }}>
+//         <input className="px-4px rounded py-3 text-xl m-5 bg-white text-black" type="text" placeholder="Enter the Name"/>
+//         <button className="px-4px py-3 w-50 text-xl text-white bg-emerald-600 rounded">Upload</button>
+//       </form>
+//     </div>
+//   )
+// }
+
+
+
 
 
 
