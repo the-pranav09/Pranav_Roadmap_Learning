69afbec503f57808d1b551a837e96bbca94db70c
Pranav Ghadge <psghadge09@gmail.com>
2026-07-20 15:27:04 +0530

    create ReactApp Final setup

---
+++ ReactApp/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-blue-600 font-bold">Hello Tailwind</h1>
+    </div>
+  )
+}
+
+export default App
