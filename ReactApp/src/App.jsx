aad708eb0d44946f4c2f19f44c946bde256ffb46
Pranav Ghadge <psghadge09@gmail.com>
2026-07-20 16:11:31 +0530

    use preventDefault method to stop default behavior of browser

--- ReactApp/src/App.jsx
+++ ReactApp/src/App.jsx
@@ -35,11 +35,20 @@ import { useState } from "react";
 // Form Handling ----------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
 export const App = () => {
+
+  const submitHandler = (e) => {
+    e.preventDefault() // 'preventDefault()' method that is part of 'Event interface' and use for "stop" browser default behavior.(browser ke default behavior ko band krta hai).
+
+    console.log("Hare Krishna")
+  }
+
   return (
     <div>
-      <form action="">
-        <input className="px-4px py-3" type="text" placeholder="Enter the Name"/>
-        <button className="px-4px py-3 text-xl font-monospace">Upload</button>
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }}>
+        <input className="px-4px rounded py-3 text-xl m-5 bg-white text-black" type="text" placeholder="Enter the Name"/>
+        <button className="px-4px py-3 w-50 text-xl text-white bg-emerald-600 rounded">Upload</button>
       </form>
     </div>
   )
