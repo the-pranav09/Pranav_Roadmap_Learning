2bbbb694f2c46b8f9bfa3641ffc74ffc2fc3d2f5
Pranav Ghadge <psghadge09@gmail.com>
2026-07-21 19:06:24 +0530

    create component for learn props

--- ReactApp/src/App.jsx
+++ ReactApp/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Props_React from './components/props_react';
 
 const App = () => {
   return (
     <div>
         <Navbar /> {/* Import 'Navbar' component. that reduse the cluter and that component we can reuse.*/}
         <Footer />
+        
+        <Props_React a="Shreeman Legend"/>
     </div> 
   )
 }
---
+++ ReactApp/src/components/Props_React.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Props_React = (props) => {
+  return (
+    <div>The Co-Founder of Team Apex Gaming {props.a} </div>
+  )
+}
+
+export default Props_React
