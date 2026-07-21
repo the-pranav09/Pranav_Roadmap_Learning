114ef89997a488ddfdcd22b5ae551e051ecc7ffc
Pranav Ghadge <psghadge09@gmail.com>
2026-07-21 17:08:51 +0530

    add footer component in app.jsx file

---
+++ ReactApp/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className=' flex w-full bg-black text-bold text-orange-500 py-5 absolute bottom-0 items-center justify-center'>Welcome To Gaming Den</div>
+  )
+}
+
+export default Footer
--- ReactApp/src/components/Navbar.jsx
+++ ReactApp/src/components/Navbar.jsx
@@ -3,13 +3,15 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-        <nav className=' bg-black flex py-5 px-10 items-center justify-between'>
-            <h2 className='text-2xl'>Gaming Den</h2>
+        <nav className=' bg-black text-orange-500 flex py-5 px-10 items-center justify-between'>
+
+            <h2 className='text-2xl font-bold text-orange-400'>Gaming Den</h2>
+
             <div className='flex gap-8 items-center'>
-                <h4 className='text-xl'>Home</h4>
-                <h4 className='text-xl'>Game</h4>
-                <h4 className='text-xl'>Tournament</h4>
-                <h4 className='text-xl'>Community</h4>
+                <h4 className='text-xl hover:text-white'>Home</h4>
+                <h4 className='text-xl hover:text-white'>Game</h4>
+                <h4 className='text-xl hover:text-white'>Tournament</h4>
+                <h4 className='text-xl hover:text-white'>Community</h4>
             </div>
         </nav>
     </div>
