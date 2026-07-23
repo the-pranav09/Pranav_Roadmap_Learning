647fb6f49bfce8751cdc5bc5c8744413c57b420d
Pranav Ghadge <psghadge09@gmail.com>
2026-07-23 21:59:27 +0530

    learn .this keyword that contain the object referance

--- TypeScript/classes.js
+++ TypeScript/classes.js
@@ -2,28 +2,22 @@
 // 'class' ek blueprint hai.
 // uss blueprint se hum multiple object bana sakte hai.
 // har ek object ke paas apni ek properties(data) hoti hai.
+// class ke andar agar function create kiya to usee method kaha jata hai.
 // also known as 'class constructor'.
-class PS_5 {
-    Game;
-    Game_Type;
-    Price;
-    // 'constructor' ek special function hai jo object banate time chalta hai. 
-    // and properties ko initialize krta hai.(properties ko data deta hai).
-    // constructor object bane ke baad immediately run hota hai.
-    constructor(Game, Game_Type, Price) {
-        this.Game = Game;
-        this.Game_Type = Game_Type;
-        this.Price = Price;
-    }
-}
+// class PS_5{ // 'PS_5' is class name.
+//     // 'constructor' ek special function hai jo object banate time chalta hai. 
+//     // and properties ko initialize krta hai.(properties ko data deta hai).
+//     // constructor object bane ke baad immediately run hota hai.
+//     constructor(public Game:string, public Game_Type:string, public Price:number){}
+// }
 // 'G1' is object hai jo "PS_5" ke class se banaya gaya hai.
 // and object ke andar data store kiya hai.
-let G1 = new PS_5("Uncharted", "Action_Adventure", 9999);
-console.log(G1);
-let G2 = new PS_5("Last of Us", "Action", 5999); // multiple object.
-console.log(G2);
-let G3 = new PS_5("Wolverine", "Action", 7999);
-console.log(G3);
+// let G1 = new PS_5("Uncharted", "Action_Adventure", 9999)
+// console.log(G1)
+// let G2 = new PS_5("Last of Us", "Action", 5999) // multiple object.
+// console.log(G2)
+// let G3 = new PS_5("Wolverine", "Action", 7999)
+// console.log(G3)
 // class Music{
 //     constructor(public name:string, public artist:string, public thumbnail:string, public length:number, public free:boolean){}
 // }
@@ -31,3 +25,23 @@ console.log(G3);
 // console.log(audio_1)
 // let audio_2 = new Music("Nee Singam Dhan", "Sid Sriram and Vivek", "", 4.7, false)
 // console.log(audio_2)
+// '.this'----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// '.this' is use referance.('this' ke andar referance data store hota hai)
+// class abc {
+//     names ="vyala";
+//     changeName(){
+//         console.log(this.names)
+//         console.log(this.random)        
+//     }
+//     random(){
+//         console.log("lalala laaaa")
+//     }
+// }
+class lap {
+    lapName;
+    constructor(lapName) {
+        this.lapName = lapName;
+    }
+}
+let l1 = new lap("LOQ");
+console.log(l1);
